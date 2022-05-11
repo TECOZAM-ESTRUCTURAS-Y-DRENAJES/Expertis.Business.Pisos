353f1826fe3207e01e9d9dcafe8a838a0ea6bb5a
SergioTecozam <sblanco@tecozam.com>
2022-05-11 10:48:36 +0200

    Initial commit

---
+++ Expertis.Business.Pisos/ContratosPisos.vb
@@ -0,0 +1,49 @@
+﻿Imports Solmicro.Expertis
+Imports Solmicro.Expertis.Engine.BE.BusinessProcesses
+Imports Solmicro.Expertis.Engine.DAL
+
+Public Class ContratosPisos
+
+    Inherits Solmicro.Expertis.Engine.BE.BusinessHelper
+
+    Public Sub New()
+        MyBase.New(cnEntidad)
+    End Sub
+
+    Private Const cnEntidad As String = "tbPisosContrato"
+
+    Protected Overrides Sub RegisterAddnewTasks(ByVal addnewProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterAddnewTasks(addnewProcess)
+        addnewProcess.AddTask(Of DataRow)(AddressOf FillDefaultValues)
+    End Sub
+
+    <Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
+        ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarValoresPredeterminados, data, services)
+        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarCentroGestion, data, services)
+        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarContador, data, services)
+    End Sub
+
+    <Task()> Public Shared Sub AsignarValoresPredeterminados(ByVal data As DataRow, ByVal services As ServiceProvider)
+        data("IDPisoContrato") = AdminData.GetAutoNumeric
+    End Sub
+
+    Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterUpdateTasks(updateProcess)
+        updateProcess.AddTask(Of DataRow)(AddressOf AsignarValoresPredeterminados)
+    End Sub
+
+    Public Sub EjecutarSql(ByVal sql As String)
+
+        AdminData.Execute(sql)
+
+    End Sub
+
+    Function EjecutarSqlSelect(ByVal sql As String)
+
+        Dim cadena As DataTable
+        cadena = AdminData.GetData(sql)
+        Return cadena
+
+    End Function
+
+End Class
---
+++ Expertis.Business.Pisos/PisoContacto.vb
@@ -0,0 +1,34 @@
+﻿Imports Solmicro.Expertis
+Imports Solmicro.Expertis.Engine.BE.BusinessProcesses
+Imports Solmicro.Expertis.Engine.DAL
+Public Class PisoContacto
+
+    Inherits Solmicro.Expertis.Engine.BE.BusinessHelper
+
+    Public Sub New()
+        MyBase.New(cnEntidad)
+    End Sub
+
+    Private Const cnEntidad As String = "tbPisoContacto"
+
+    Protected Overrides Sub RegisterAddnewTasks(ByVal addnewProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterAddnewTasks(addnewProcess)
+        addnewProcess.AddTask(Of DataRow)(AddressOf FillDefaultValues)
+    End Sub
+
+    <Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
+        ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarValoresPredeterminados, data, services)
+        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarCentroGestion, data, services)
+        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarContador, data, services)
+    End Sub
+
+    <Task()> Public Shared Sub AsignarValoresPredeterminados(ByVal data As DataRow, ByVal services As ServiceProvider)
+        data("IDPisoContacto") = AdminData.GetAutoNumeric
+    End Sub
+
+    Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterUpdateTasks(updateProcess)
+        updateProcess.AddTask(Of DataRow)(AddressOf AsignarValoresPredeterminados)
+    End Sub
+
+End Class
