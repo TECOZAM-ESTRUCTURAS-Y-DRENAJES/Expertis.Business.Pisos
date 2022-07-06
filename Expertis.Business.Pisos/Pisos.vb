9b6b4c04b8d18f35f6d965066a97c57fc5c41b44
DavidVelascoHerrero1 <david.velasco@tecozam.com>
2022-07-06 10:47:19 +0200

    Restricciones de guardado

--- Expertis.Business.Pisos/Pisos.vb
+++ Expertis.Business.Pisos/Pisos.vb
@@ -20,11 +20,12 @@ Public Class Pisos
 
     <Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
         ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarValoresPredeterminados, data, services)
-
     End Sub
 
     <Task()> Public Shared Sub AsignarValoresPredeterminados(ByVal data As DataRow, ByVal services As ServiceProvider)
         data("IDPiso") = AdminData.GetAutoNumeric
+        data("Activo") = True
+        data("Bloqueado") = False
     End Sub
     <Task()> Public Shared Sub checkObligaciones(ByVal data As DataRow, ByVal services As ServiceProvider)
         If data("IVAs").ToString.Length = 0 Then
@@ -33,11 +34,32 @@ Public Class Pisos
         If data("Vencimiento").ToString.Length = 0 Then
             ApplicationService.GenerateError("Seleccione el tipo de mes, vencido o adelantado")
         End If
+
+    End Sub
+    <Task()> Public Shared Sub checkObligaciones2(ByVal data As DataRow, ByVal services As ServiceProvider)
+
+        Try
+            Dim cadena As DataTable
+            Dim sql As String
+            Dim cod As String
+            cod = data("Codigo")
+            sql = "select * from tbMaestroPisos where Codigo='" & cod & "'"
+            cadena = AdminData.GetData(sql)
+            If cadena.Rows.Count <> 0 Then
+                ApplicationService.GenerateError("Ya existe un piso con este nombre(" & cod & ").")
+            End If
+        Catch ex As Exception
+
+        End Try
+        
+        'Tambien pone el combo a true
+
     End Sub
 
     Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
         MyBase.RegisterUpdateTasks(updateProcess)
         updateProcess.AddTask(Of DataRow)(AddressOf checkObligaciones)
+
         'updateProcess.AddTask(Of DataRow)(AddressOf AsignarValoresPredeterminados)
     End Sub
 
