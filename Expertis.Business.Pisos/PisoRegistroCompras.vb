af2dee86566690f66b80af04400b1fa1aeac8d2f
David Velasco <david.velasco@tecozam.com>
2025-02-11 18:33:00 +0100

    Clase PisoRegistroCompras

---
+++ Expertis.Business.Pisos/PisoRegistroCompras.vb
@@ -0,0 +1,53 @@
+﻿Imports Solmicro.Expertis.Engine.BE
+Imports Solmicro.Expertis
+Imports Solmicro.Expertis.Engine.BE.BusinessProcesses
+Imports Solmicro.Expertis.Engine.DAL
+
+Public Class PisoRegistroCompras
+    Inherits Solmicro.Expertis.Engine.BE.BusinessHelper
+
+    Public Sub New()
+        MyBase.New(cnEntidad)
+    End Sub
+
+    Private Const cnEntidad As String = "tbComprasPisos"
+
+#Region "RegisterAddNewTasks"
+    Protected Overrides Sub RegisterAddnewTasks(ByVal addnewProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterAddnewTasks(addnewProcess)
+        addnewProcess.AddTask(Of DataRow)(AddressOf FillDefaultValues)
+    End Sub
+
+    <Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
+        ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarClavePrimaria, data, services)
+    End Sub
+
+    <Task()> Public Shared Sub AsignarClavePrimaria(ByVal data As DataRow, ByVal services As ServiceProvider)
+        data("IDLineaRegistro") = AdminData.GetAutoNumeric
+    End Sub
+#End Region
+
+#Region " RegisterValidateTasks "
+
+    Protected Overrides Sub RegisterValidateTasks(ByVal validateProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterValidateTasks(validateProcess)
+    End Sub
+
+
+#End Region
+
+#Region "Eventos RegisterDeleteTasks"
+    Protected Overrides Sub RegisterDeleteTasks(ByVal DeleteProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterDeleteTasks(DeleteProcess)
+
+    End Sub
+#End Region
+
+#Region "Eventos RegisterUpdateTasks"
+
+    Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterUpdateTasks(updateProcess)
+    End Sub
+#End Region
+
+End Class
